614780cf451c64df801c06c5b2482bd4314c5207
gensuruh <tanakaphper@gmail.com>
2023-01-19 23:00:20 +0900

    Add ServiceProvider test

---
+++ tests/Providers/SdpgsServiceProviderTest.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Config\Repository;
+use Sdpgs\Gyazo\GyazoClient;
+use Sdpgs\Gyazo\GyazoException;
+use Sdpgs\GyazoLaravel\Providers\SdpgsGyazoServiceProvider;
+
+test('example', function () {
+    expect(true)->toBeTrue();
+});
+
+it('binds the client on the container', function () {
+    $app = app();
+
+    $app->bind('config', fn () => new Repository([
+        'gyazo' => [
+            'gyazo_access_token' => 'test',
+        ],
+    ]));
+
+    (new SdpgsGyazoServiceProvider($app))->register();
+
+    expect($app->get(GyazoClient::class))->toBeInstanceOf(GyazoClient::class);
+});
+
+it('binds the client on the container as singleton', function () {
+    $app = app();
+
+    $app->bind('config', fn () => new Repository([
+        'gyazo' => [
+            'gyazo_access_token' => 'test',
+        ],
+    ]));
+
+    (new SdpgsGyazoServiceProvider($app))->register();
+
+    $gyazoClient = $app->get(GyazoClient::class);
+
+    expect($app->get(GyazoClient::class))->toBe($gyazoClient);
+});
+
+it('requires an access token', function () {
+    $app = app();
+
+    $app->bind('config', fn () => new Repository([]));
+
+    (new SdpgsGyazoServiceProvider($app))->register();
+})->throws(
+    GyazoException::class,
+    'Gyazo access token is incorrect.'
+);
+
+it('provides', function () {
+    $app = app();
+
+    $provides = (new SdpgsGyazoServiceProvider($app))->provides();
+
+    expect($provides)->toBe([GyazoClient::class]);
+});
