bb571ea582224ac9f075e5fc3ee375b2c511a083
gensuruh <tanakaphper@gmail.com>
2023-01-19 22:59:09 +0900

    Add config

---
+++ config/gyazo.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Configures Gyazo Access Token
+    |--------------------------------------------------------------------------
+    |
+    | Here you can set Gyazo access token. Gyazo access token is used to
+    | authenticate with the Gyazo API - you can find your Gyazo Access Token
+      on your Gyazo dashboard, at https://gyazo.com.
+    */
+
+    'gyazo_access_token' => env('GYAZO_ACCESS_TOKEN'),
+
+];
